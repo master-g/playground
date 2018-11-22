ffddf1d63d7858fd05fef29271263d3799fd440d
mg <master-g@users.noreply.github.com>
2018-11-23 01:19:18 +0800

    update

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"runtime"
+	"math/rand"
 
-	"github.com/master-g/playground/internal/abyss"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	runtime.LockOSThread()
+	poolorder := make([]int, 8)
+	// loop through slice
+	for i := 1; i < len(poolorder); i++ {
+		j := rand.Int31n(int32(i + 1)) // choose a random index between 0~i
+		poolorder[i] = poolorder[j]    // swap slice[i] and slice[j]
+		poolorder[j] = i               // slice[j] = i
+	}
 
-	abyss.Entry()
+	logrus.Info(poolorder)
 }
