a01ca7aa400dfce2e72022343bf67791d920305e
mg <master-g@users.noreply.github.com>
2018-12-17 20:30:55 +0800

    update

--- internal/l337c0d3/question2.go
+++ internal/l337c0d3/question2.go
@@ -6,5 +6,31 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return nil
+	n1 := l1
+	n2 := l2
+	var result *ListNode
+	r := result
+	for {
+		if n1 == nil || n2 == nil {
+			break
+		}
+
+		sum := n1.Val + n2.Val
+		overflow := 0
+		if sum >= 10 {
+			overflow = 1
+		}
+		r = &ListNode{
+			Val:  sum % 10,
+			Next: nil,
+		}
+		n1 = n1.Next
+		n2 = n2.Next
+		r.Next = &ListNode{
+			Val:  overflow,
+			Next: nil,
+		}
+		r = r.Next
+	}
+	return result
 }
