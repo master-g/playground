dd571f5e67664ca0331a6c36231dac75dbf1849a
mg <master-g@users.noreply.github.com>
2018-12-19 00:38:03 +0800

    update

--- internal/l337c0d3/question3.go
+++ internal/l337c0d3/question3.go
@@ -1,5 +1,24 @@
 package l337c0d3
 
 func lengthOfLongestSubstring(s string) int {
-	return 0
+	set := make(map[byte]bool)
+	max := 0
+	b := []byte(s)
+	for i := 0; i < len(b)-1; {
+		curLen := 0
+		set[b[i]] = true
+		for j := i + 1; j < len(b); j++ {
+			if set[b[j]] {
+				if curLen > max {
+					// update max if needed
+					max = curLen
+				}
+				// reset map
+				set = make(map[byte]bool)
+				// TODO
+				break
+			}
+		}
+	}
+	return max
 }
